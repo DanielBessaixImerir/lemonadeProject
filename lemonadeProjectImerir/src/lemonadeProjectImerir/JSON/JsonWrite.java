849e0d17ff93306b9378572f4a5587fb64276149
Aymeric <Aymeric@Aymeric-PC>
2017-06-21 11:17:50 +0200

    JSON

--- lemonadeProjectImerir/src/lemonadeProjectImerir/JSON/JsonWrite.java
+++ lemonadeProjectImerir/src/lemonadeProjectImerir/JSON/JsonWrite.java
@@ -1,5 +1,15 @@
 package lemonadeProjectImerir.JSON;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import org.json.*;
+import java.io.*;
 
 public class JsonWrite {
-
+	public static void main(String[] arg){
+		
+			
+		}
+	}
 }
