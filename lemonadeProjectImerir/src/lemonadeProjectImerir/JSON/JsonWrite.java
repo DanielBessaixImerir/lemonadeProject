16cd3591f36c56a40207180d860837b95af1b59a
Einlad <Einlad@172.30.1.203>
2017-06-21 11:35:24 +0200

    main de recuperation de donnée a partir d'un url (dans la class main)

--- lemonadeProjectImerir/src/lemonadeProjectImerir/JSON/JsonWrite.java
+++ lemonadeProjectImerir/src/lemonadeProjectImerir/JSON/JsonWrite.java
@@ -3,13 +3,12 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Iterator;
-import org.json.*;
 import java.io.*;
 
 public class JsonWrite {
 	public static void main(String[] arg){
 		
 			
-		}
+		
 	}
 }
