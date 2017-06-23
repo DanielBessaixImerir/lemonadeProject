30e16d8eb770cfe8e20e2bc3568bda52b6ae9771
Einlad <Einlad@172.30.1.203>
2017-06-23 17:35:01 +0200

    constructeur Player avec Json

--- lemonadeProjectImerir/src/lemonadeProjectImerir/Player.java
+++ lemonadeProjectImerir/src/lemonadeProjectImerir/Player.java
@@ -1,9 +1,25 @@
 package lemonadeProjectImerir;
 
+import java.util.ArrayList;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
 public class Player {
 	private String namePlayer;
-	private int IDPlayer;
 	private int cash;
 	private int sales;
-	private int profit;
+	private float profit;
+	private ArrayList<Drink> drinks;
+	
+	public Player(String name, JsonObject json){
+		this.cash = json.get("cash").getAsInt();
+		this.sales = json.get("sales").getAsInt();
+		this.profit = json.get("profit").getAsFloat();
+		this.drinks = new ArrayList<Drink>();
+		JsonArray jsonArrayDrink = json.get("drinksOffered").getAsJsonArray();
+		for (int i=0;i<jsonArrayDrink.size();i++){
+			this.drinks.add(new Drink(jsonArrayDrink.get(i).getAsJsonObject()));
+		}
+	}
 }
