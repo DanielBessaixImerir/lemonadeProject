2bf40d74c2f696feefd4605c2dcc935d20e88249
Einlad <Einlad@172.30.1.203>
2017-06-23 15:36:40 +0200

    stringToJson

--- lemonadeProjectImerir/src/lemonadeProjectImerir/JSON/JsonWrite.java
+++ lemonadeProjectImerir/src/lemonadeProjectImerir/JSON/JsonWrite.java
@@ -1,5 +1,9 @@
 package lemonadeProjectImerir.JSON;
 
+import org.json.JSONObject;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 
 public class JsonWrite {
 		public static String SaleRequest(String player, String drink, int quantity){
@@ -10,5 +14,12 @@ public class JsonWrite {
 	                "}]";
 			return ret;
 		}
+		
+		public static JsonObject stringToJson(String jsonString){
+			String string = "abcde"; // The String You Need To Be Converted 
+			JsonObject convertedObject = new Gson().fromJson(string, JsonObject.class);
+			return convertedObject;
+		}
+		
 			
 	}
