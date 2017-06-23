a8710000dcde1cafdd7dffec04b7636699d896c2
Aymeric <Aymeric@Aymeric-PC>
2017-06-23 11:20:57 +0200

    library add

--- lemonadeProjectImerir/src/lemonadeProjectImerir/JSON/Main.java
+++ lemonadeProjectImerir/src/lemonadeProjectImerir/JSON/Main.java
@@ -3,11 +3,14 @@ package lemonadeProjectImerir.JSON;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import com.google.gson.*;
 
 public class Main {
 
 	public static void main(String[] args) {
 		//getPersonnes();
 		HtmlPOST.sendJSON("https://tranquil-reef-75630.herokuapp.com/test");
+		Gson g = new GsonBuilder().create();
 	}
+	
 }
