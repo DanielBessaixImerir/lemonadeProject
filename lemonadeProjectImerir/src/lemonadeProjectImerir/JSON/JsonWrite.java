d7117fcb4e60a29b8f092891a15def2005daa549
Einlad <Einlad@172.30.1.203>
2017-06-23 12:15:03 +0200

    :ol

--- lemonadeProjectImerir/src/lemonadeProjectImerir/JSON/JsonWrite.java
+++ lemonadeProjectImerir/src/lemonadeProjectImerir/JSON/JsonWrite.java
@@ -1,12 +1,14 @@
 package lemonadeProjectImerir.JSON;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Iterator;
-import java.io.*;
+
 
 public class JsonWrite {
-	public static void main(String[] arg){
-	
+		public static String SaleRequest(String player, String drink, int quantity){
+			String ret = "[{" +
+	                "\"player\":\""+player+"\"," +
+	                "\"item\":\""+drink+"\"," +
+	                "\"quantity\":\""+quantity+"\"" +
+	                "}]";
+			return ret;
 		}
+			
 	}
--- lemonadeProjectImerir/src/lemonadeProjectImerir/JSON/Main.java
+++ lemonadeProjectImerir/src/lemonadeProjectImerir/JSON/Main.java
@@ -1,16 +1,11 @@
 package lemonadeProjectImerir.JSON;
-
-
-import org.json.JSONException;
-import org.json.JSONObject;
-import com.google.gson.*;
-
 public class Main {
 
 	public static void main(String[] args) {
 		//getPersonnes();
-		HtmlPOST.sendJSON("https://tranquil-reef-75630.herokuapp.com/test");
-		Gson g = new GsonBuilder().create();
+		//HtmlPOST.sendJSON("https://tranquil-reef-75630.herokuapp.com/");
+		//Gson g = new GsonBuilder().create();
+		System.out.println(JsonWrite.SaleRequest("Daniel","margarita",3));
 	}
 	
 }
