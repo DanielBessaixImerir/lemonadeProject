04b5dafb433e193d5f66305418ebff15592dce97
Aymeric <Aymeric@Aymeric-PC>
2017-06-23 17:34:53 +0200

    Constructeur MapItem et Player

--- lemonadeProjectImerir/src/lemonadeProjectImerir/MapItem.java
+++ lemonadeProjectImerir/src/lemonadeProjectImerir/MapItem.java
@@ -2,8 +2,20 @@ package lemonadeProjectImerir;
 
 import java.util.ArrayList;
 
+import com.google.gson.JsonObject;
+
 public class MapItem {
-	private Coord coord;
+	private Coord location;
 	private KindItem kind;
-	private ArrayList<Drink> drinks;
+	private float influence;
+	
+	public MapItem(JsonObject json){
+		this.location = new Coord(json.get("location").getAsJsonObject(),false);
+		if(json.get("Kind").getAsString().equals(KindItem.AD.toString())){
+			this.kind = KindItem.AD;
+		}else{
+			this.kind = KindItem.STAND;
+		}
+		this.influence = json.get("influence").getAsFloat();
+	}
 }
--- lemonadeProjectImerir/src/lemonadeProjectImerir/Player.java
+++ lemonadeProjectImerir/src/lemonadeProjectImerir/Player.java
@@ -1,9 +1,13 @@
 package lemonadeProjectImerir;
 
+import java.util.ArrayList;
+
 public class Player {
 	private String namePlayer;
 	private int IDPlayer;
 	private int cash;
 	private int sales;
 	private int profit;
+	private ArrayList<MapItem> mapItem;
+	private ArrayList<Drink> drinks;
 }
